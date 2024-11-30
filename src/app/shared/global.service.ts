e7bd0611c1f122108bc541acce900029e7fe05f6
atishblote <83937743+atishblote@users.noreply.github.com>
2024-11-30 22:38:31 +0530

    Update global.service.ts
    Cloud url added

--- src/app/shared/global.service.ts
+++ src/app/shared/global.service.ts
@@ -6,11 +6,11 @@ import { Injectable } from '@angular/core';
 })
 export class GlobalService {
 
-  baseUrl:string= 'https://spa-strapi-1.onrender.com/api'
-  // baseUrl:string= 'https://api.cricfootbook.com'
+  // baseUrl:string= 'https://spa-strapi-1.onrender.com/api'
+  baseUrl:string= 'https://inspired-positivity-3f2cb0cfbd.strapiapp.com/api'
 
-  globalToken:string = "70af910f02a243aaf6ce4497162d998c0fdb404e6b54e245e5b01698e19caab33490c8cff0c8b2b73f53971defc84ceca7f75d08d1e3bdbe8f6203f8d50bbaaaeab1934cd37a1851bf8600c4d9a8f9c7b51428bc0cd9f47044a275070c579e61dae31d3f7727e950f7156542b4fdd1dd63ff8a2a9b7e63fa41497ac98c07aa82"
-
+  //globalToken:string = "70af910f02a243aaf6ce4497162d998c0fdb404e6b54e245e5b01698e19caab33490c8cff0c8b2b73f53971defc84ceca7f75d08d1e3bdbe8f6203f8d50bbaaaeab1934cd37a1851bf8600c4d9a8f9c7b51428bc0cd9f47044a275070c579e61dae31d3f7727e950f7156542b4fdd1dd63ff8a2a9b7e63fa41497ac98c07aa82"
+  globalToken:string = "d500b0100bead4260f3692f9f04f7789a4c30f3ae0132366c72559c9d81f835a2f764a03b71a8d3528230ebee7ad5c85632e7e69c7ee43ad9ae25acc2521c8151847dc1b209204069c618a5652eaceb25e55fe49a8166f30161414695de045ff07c541ae1e19767b7f05d01450525d19ee929a58a51c921e3a758d509eafc623";
   apiKey:any
 
   constructor(private http: HttpClient) { }
